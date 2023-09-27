fb8f4d0750ace26a2e0142bd9aa7eff5eb49d47c
Giorgio <info@giorgiodominoni.it>
2023-09-27 12:09:34 +0200

    Fix create route for furnishing

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,11 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use Fieroo\Furnitures\Controllers\FurnishingController;
-    
+
 Route::group(['prefix' => 'admin', 'middleware' => ['web','auth']], function() {
     Route::resource('/furnishings', FurnishingController::class);
     Route::post('/furnishings/stands', [FurnishingController::class, 'getStandsList']);
     Route::post('/furnishings/stands_types', [FurnishingController::class, 'getStandType']);
+    Route::post('/furnishings/store', [FurnishingController::class, 'store'])->name('furnishings.store');
     Route::get('/furnishings/{id}/variants', [FurnishingController::class, 'indexVariant']);
     Route::get('/furnishings/{id}/variants/create', [FurnishingController::class, 'createVariant']);
     Route::get('/furnishings/{id}/variants/{variant}/edit', [FurnishingController::class, 'editVariant']);
